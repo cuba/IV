7ad75469d79c2173f1db85b131dbf46207151495
Jacob Sikorski <jacob.sikorski@workjam.com>
2020-01-12 12:54:16 -0500

    Initial Commit

---
+++ VIExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VIExample
+//
+//  Created by Jacob Sikorski on 2020-01-12.
+//  Copyright © 2020 Jacob Sikorski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
